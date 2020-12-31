5250d7ee31b51e86b7ae83372185f62cf37c7dc8
jadasc <jadal115@gmail.com>
2020-12-30 21:15:19 -0600

    Refactor: Add CircularLinkedList class

--- 23_crab-cups/23_part-one.js
+++ 23_crab-cups/23_part-one.js
@@ -2,138 +2,185 @@
  * --- Day 23: Crab Cups --- *
  *****************************/
 
-const INPUT_TEST = 389125467; // max label will be the # of digits since the labels are sequential
+const INPUT_TEST = 389125467;
 const INPUT = 925176834;
 
 const PICK_UP_COUNT = 3;
 
+/** Classes */
+
 class Node {
-    constructor(label, next) {
-        this.label = label;
+    constructor(value, next) {
+        this.value = value;
         this.next = next;
     }
 }
 
-// remove nodes from the linked list. The n adjacent nodes from the start node will be removed
-// return the head node for the extracted adjacent nodes
-function extract(start, count) {
-    // extract nodes
-    const extractedHead = start.next;
-    let end = start;
-    for (let i = 0; i < count; i++) {
-        end = end.next;
+class CircularLinkedList {
+    constructor() {
+        this.head = null;
+        this.count = 0;
     }
-    let next = end.next;
-    end.next = null;
 
-    // rejoin linked list
-    start.next = next;
-    this.count -= count;
-    return extractedHead;
+    // insert a node and make it the head
+    add(node) {
+        if (this.head === null) {
+            this.head = node;
+            this.head.next = node;
+        } else {
+            node.next = this.head.next;
+            this.head.next = node;
+            this.head = node;
+        }
+
+        this.count++;
+    }
+
+    // print linked list
+    print() {
+        let values = [];
+        let curr = this.head;
+
+        do {
+            values.push(curr.value);
+            curr = curr.next;
+        } while (curr !== this.head);
+
+        console.log(values.join("->"));
+    }
+
+    // return node with matching value
+    get(value) {
+        if (this.head.value === value) {
+            return this.head;
+        }
+
+        let curr = this.head;
+        do {
+            curr = curr.next;
+        } while (curr.next !== this.head && curr.value !== value);
+
+        if (curr === this.head) {
+            return null;
+        }
+        return curr;
+    }
+
+    // return cup values in order starting from the head
+    getValues() {
+        const values = new Set();
+
+        if (this.head !== null) {
+            let curr = this.head;
+
+            do {
+                values.add(curr.value);
+                curr = curr.next;
+            } while (curr !== this.head);
+        }
+
+        return values;
+    }
+
+    // remove nodes from the linked list. The n adjacent nodes from the start node will be removed
+    // return the head node for the extracted adjacent nodes
+    extract(start, count) {
+        // find start's preceding node
+        let prev = this.head;
+        while (prev.next !== start) {
+            prev = prev.next;
+        }
+
+        // extract nodes
+        let end = start;
+        for (let i = 0; i < count - 1; i++) {
+            end = end.next;
+        }
+        let next = end.next;
+        end.next = null;
+
+        // rejoin linked list
+        prev.next = next;
+
+        this.count -= count;
+
+        return start;
+    }
+
+    // insert nodes in linked list after the start node
+    emplace(start, emplaceHead) {
+        let prev = emplaceHead;
+        let count = 1;
+        while (prev.next !== null) {
+            prev = prev.next;
+            count++;
+        }
+        prev.next = start.next;
+        start.next = emplaceHead;
+
+        this.count += count;
+    }
+
+    // make the head point to the current head's next node
+    next() {
+        this.head = this.head.next;
+    }
+
+    // return the head's next node
+    getNext() {
+        return this.head.next;
+    }
 }
 
-// insert nodes in linked list after the start node
-function emplace(start, emplaceHead) {
-    let prev = emplaceHead;
-    while (prev.next !== null) {
-        prev = prev.next;
-    }
-    prev.next = start.next;
-    start.next = emplaceHead;
-}
+/** Functions */
 
-// return cup labels in order starting from the head
 const getLabels = (head) => {
     let curr = head;
-    const labels = new Set();
+    const values = new Set();
     while (curr !== null && curr.next !== head) {
-        labels.add(curr.label);
+        values.add(curr.value);
         curr = curr.next;
     }
 
-    return labels;
+    return values;
 };
 
-// return node with matching value
-const findCup = (head, label) => {
-    if (head.label === label) {
-        return head;
+function play(input) {
+    const values = input.toString().split("").map(Number);
+
+    const cups = new CircularLinkedList();
+
+    for (const value of values) {
+        cups.add(new Node(value, null));
     }
 
-    let curr = head;
-    while (curr.next !== head && curr.label !== label) {
-        curr = curr.next;
+    for (let i = 0; i < 100; i++) {
+        cups.next();
+        executeMove(cups);
     }
 
-    if (curr === head) {
-        return null;
-    }
-    return curr;
-};
+    console.log([...cups.getValues()].join(""));
+}
 
-const executeMove = (selectedCup, maxCupLabel) => {
+const executeMove = (cups) => {
     // pick up cups
-    const pickedUpCups = extract(selectedCup, PICK_UP_COUNT);
-    const extractedLabels = getLabels(pickedUpCups);
+    const pickedUpCups = cups.extract(cups.getNext(), PICK_UP_COUNT);
+    const pickedUpLabels = getLabels(pickedUpCups);
 
     // select destination cup
-    let destinationCupLabel = selectedCup.label;
+    let destinationCupLabel = cups.head.value;
 
     do {
         destinationCupLabel--;
         if (destinationCupLabel < 1) {
-            destinationCupLabel = maxCupLabel;
+            destinationCupLabel = cups.count + PICK_UP_COUNT;
         }
-    } while (extractedLabels.has(destinationCupLabel));
+    } while (pickedUpLabels.has(destinationCupLabel));
 
-    const destinationCup = findCup(selectedCup, destinationCupLabel);
+    const destinationCup = cups.get(destinationCupLabel);
 
     // place picked up cups
-    emplace(destinationCup, pickedUpCups);
-
-    return selectedCup.next;
+    cups.emplace(destinationCup, pickedUpCups);
 };
-
-// print (circular) linked list
-function print(head) {
-    let values = [];
-
-    let curr = head;
-    do {
-        values.push(curr.label);
-        curr = curr.next;
-    } while (curr !== null && curr !== head);
-
-    console.log(values.join("->"));
-}
-
-function play(input) {
-    const cups = input.toString().split("").map(Number);
-
-    let prev, next;
-    let head;
-
-    for (const cup of cups) {
-        next = new Node(cup, null);
-
-        if (!prev) {
-            head = next;
-        } else {
-            prev.next = next;
-        }
-
-        prev = next;
-    }
-
-    // create circular list
-    prev.next = head;
-
-    let selectedCup = head;
-    for (let i = 0; i < 100; i++) {
-        selectedCup = executeMove(selectedCup, cups.length);
-    }
-
-    print(findCup(selectedCup, 1));
-}
 
 play(INPUT);
